2ca2bf2770ba2771b1c34ed98553ea0e1b95e56c
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-13 02:24:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, Grace Hopper, currentUser';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ';
+const welcomeMessage.substr(-1).'!';
